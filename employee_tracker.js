c352bf9554251f58eebba7f516a9095d328836f2
Priya Garg <gupta.ria100@gmail.com>
2021-01-06 00:17:27 -0600

    adding basic code

---
+++ employee_tracker.js
@@ -0,0 +1,157 @@
+var mysql = require("mysql");
+const inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // My port
+  port: 3306,
+
+  // My username
+  user: "root",
+
+  // My password
+  password: "priyapassword",
+  database: "employee_tracker_db"
+});
+
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId + "\n");
+  start();
+
+});
+
+
+function start() {
+  inquirer.prompt({
+    name: "actions",
+    type: "list",
+    message: "What do you want to do?",
+    choices: [
+      "View All Employees?",  
+      "View All Employees By Roles?",
+      "View all Employee By Deparments?",
+      "Update Employee?",
+      "Add Employee?",
+      "Add Role?",
+      "Add Department?"
+    ]
+
+  }).then(function (answer) {
+    switch(answer.actions){
+      case "View All Employees?":
+        console.log("in switch statement");
+        ViewEmployees();
+        break;
+        case "View All Employees By Roles?":
+        ViewEmployeesByRoles();
+        break;
+        case "View all Employees By Deparments?":
+        ViewEmployeesByDepartments();
+        break;
+        case "Update Employees?":
+        UpdateEmployee();
+        break;
+        case "Add Employee?":
+          AddEmployee();
+        break;
+        case "Add Role?":
+          AddRole();
+        break;
+        case "Add Department?":
+          AddDepartment();
+        break;
+
+    }
+  
+  });
+}
+
+function ViewEmployees() {
+  console.log("success");
+   connection.query("SELECT * FROM employee", function(err, res) {
+    if (err) throw err;
+    // Log all results of the SELECT statement
+    console.table(res);
+    connection.end();
+  });
+  // start();
+}
+
+function ViewEmployeesByRoles(){
+  console.log(" i am in ViewEmployeesByRoles");
+  start();
+}
+
+function ViewEmployeesByDepartments(){
+  console.log(" i am in ViewEmployeesByDepartments");
+  start();
+}
+
+// function createProduct() {
+//   console.log("Inserting a new product...\n");
+//   var query = connection.query(
+//     "INSERT INTO auctions SET ?",
+//     {    
+//   item_name:"T-shirt",
+//   category :"clothing",
+//   starting_bid :30,
+//   highest_bid :40
+//     },
+//     function(err, res) {
+//       if (err) throw err;
+//       console.log(res.affectedRows + " product inserted!\n");
+//       // Call updateProduct AFTER the INSERT completes
+//       updateProduct();
+//     }
+//   );
+
+//   // logs the actual query being run
+//   console.log(query.sql);
+// }
+
+// function updateProduct() {
+//   console.log("Updating all T-shitrs  starting bid ...\n");
+//   var query = connection.query(
+//     "UPDATE auctions SET ? WHERE ?",
+//     [
+//       {
+//         starting_bid: 20
+//       },
+//       {
+//         item_name: "T-shirt"
+//       }
+//     ],
+//     function(err, res) {
+//       if (err) throw err;
+//       console.log(res.affectedRows + " products updated!\n");
+//       // Call deleteProduct AFTER the UPDATE completes
+//       deleteProduct();
+//     }
+//   );
+
+//   // logs the actual query being run
+//   console.log(query.sql);
+// }
+
+// function deleteProduct() {
+//   console.log("Deleting all bids having id 1 ...\n");
+//   connection.query(
+//     "DELETE FROM auctions WHERE ?",
+//     {
+//       id:1 
+//     },
+//     function(err, res) {
+//       if (err) throw err;
+//       console.log(res.affectedRows + " products deleted!\n");
+//       // Call readProducts AFTER the DELETE completes
+//       readProducts();
+//     }
+//   );
+// }
+
+
+
+
+
---
+++ employee_tracker_db.sql
@@ -0,0 +1,63 @@
+-- employee_tracker_db
+-- employee_tracker.js
+DROP DATABASE IF EXISTS employee_tracker_db;
+CREATE DATABASE employee_tracker_db;
+USE employee_tracker_db;
+
+CREATE TABLE department(
+  department_id INTEGER(30) NOT NULL AUTO_INCREMENT,
+  department_name VARCHAR(30),
+  PRIMARY KEY(department_id)
+);
+
+CREATE TABLE role(
+role_id INTEGER(30) NOT NULL AUTO_INCREMENT,
+title VARCHAR(30) NOT NULL,
+department_id INTEGER(30),
+salary decimal(10,2) ,
+PRIMARY KEY(role_id),
+FOREIGN KEY (department_id) REFERENCES department(department_id)
+);
+
+CREATE TABLE employee(
+employee_id INTEGER(30) NOT NULL AUTO_INCREMENT,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+role_id INTEGER(30),
+PRIMARY KEY(employee_id),
+FOREIGN KEY (role_id) REFERENCES role(role_id)
+);
+
+-- INSERTING VALUES IN DEPARTMENT TABLE for 4 departments_ID's
+INSERT INTO department (department_name)
+VALUES("Engineering"),("Finance"),("Legal"),("Sales");
+
+-- INSERTING VALUES IN ROLE TABLE for 8 ROLE_ID'S
+INSERT INTO role(title,salary,department_id)
+VALUES ("Sales Lead", 100000,3),("Lead Engineer", 150000,3),("Software Engineer", 120000,3),("Accountant", 125000,3),("Legal Team Lead", 250000,3),("Sales person", 80000,3),("Lawyer", 190000,3);
+
+
+
+-- INSERTING VALUES IN EMPLOYEE TABLE FOR 7 EMPLOYEE ID'S
+INSERT INTO employee(first_name,last_name,role_id)
+VALUES ('John', 'Doe',2);
+INSERT INTO employee(first_name,last_name,role_id)
+VALUES ('Mike', 'Chan',5);
+INSERT INTO employee(first_name,last_name,role_id)
+VALUES ('Ashley', 'Rodriguez',3);
+INSERT INTO employee(first_name,last_name,role_id)
+VALUES ('Kevin', 'Tupik',4);
+INSERT INTO employee(first_name,last_name,role_id)
+VALUES ('Malia', 'Brown',6);
+INSERT INTO employee(first_name,last_name,role_id)
+VALUES ('Sarah', 'Lourd',1);
+INSERT INTO employee(first_name,last_name,role_id)
+VALUES ('Tom', 'Allen',6);
+
+--TABLES IN OUR SQL WORKBENCH 
+SELECT * FROM department;
+SELECT * FROM role;
+SELECT * FROM employee;
+
+
+
